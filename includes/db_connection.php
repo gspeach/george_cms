994f73d71f7be2d831af303fbefe0cd777d912ac
George Speach <georgebspeach@gmail.com>
2015-02-15 23:03:47 -0500

    Connection Information
    Removing connection information.

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,8 +1,8 @@
 <?php
 	define("DB_SERVER", "localhost");
-	define("DB_USER", "php");
-	define("DB_PASS", "havefun39!");
-	define("DB_NAME", "george_cms");
+	define("DB_USER", "username");
+	define("DB_PASS", "password!");
+	define("DB_NAME", "dbname");
 
 	// 1. Create a database connection
 	$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
